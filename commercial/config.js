2b1193eebb197b3a138b1cac9e7a3c438c623684
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-14 15:42:29 +0200

    Add commercial app configuration scaffold

---
+++ commercial/config.js
@@ -0,0 +1,8 @@
+window.FLORINGO_TRAVEL_CONFIG = {
+  productName: 'FlorinGo Travel App',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  redirectUrl: window.location.origin + window.location.pathname,
+  defaultAccent: '#ff5a1f',
+  supportEmail: 'floringotravel@gmail.com'
+};
