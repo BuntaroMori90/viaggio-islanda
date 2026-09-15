04468be75aa4aa25d2b0b8e9fb01680106ca7752
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-15 09:23:30 +0200

    Improve Android PWA installation guidance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'islanda2026-v28';
+const CACHE_NAME = 'islanda2026-v29';
 const RESTYLE_CSS = './visual-restyle.css';
 const RESTYLE_JS = './visual-restyle.js';
 const HERO_CSS = './hero-restyle.css';
@@ -27,6 +27,7 @@ const CORE_ASSETS = [
   './icon-192.png',
   './icon-512.png',
   './push-notifications.js',
+  './pwa-install.js',
   './visual-restyle.css',
   './visual-restyle.js',
   './hero-restyle.css',
