bb428d3083c5b372847521854265ae5c22b2bac9
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-25 10:26:33 +0200

    Refresh PWA cache for insurance wallet update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'islanda2026-v39';
+const CACHE_NAME = 'islanda2026-v40';
 const RESTYLE_CSS = './visual-restyle.css';
 const RESTYLE_JS = './visual-restyle.js';
 const HERO_CSS = './hero-restyle.css';
